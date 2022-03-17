3b8e8a24113d8d68f6059fd1e839ba14b0b27bc5
pranav-bhalerao <pranavrajesh.bhalerao2019@vitstudent.ac.in>
2022-03-17 21:52:33 +0530

    optimizing code

--- src/components/Notes/Notes.component.jsx
+++ src/components/Notes/Notes.component.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Notes.styles.css'
-import notes from '../../db.json'
 import axios from 'axios';
 
 export default class Notes extends Component {
